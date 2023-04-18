17837381c274125de105c6d97befb87e7dd55152
Jerast <jerast@github.com>
2023-04-18 15:24:58 -0500

    [PostList, TaskList] Updated

--- TaskList/server/src/app.js
+++ TaskList/server/src/app.js
@@ -5,11 +5,9 @@ import tasksRoutes from './routes/tasks.routes.js';
 
 const app = express();
 
-app.use(
-	cors({
-		origin: 'http://localhost:5173',
-	})
-);
+app.use(cors());
+// app.use(cors({ origin: 'http://localhost:5173', }));
+// app.use(cors({ origin: 'http://192.168.0.252:5173', }));
 app.use(express.json());
 
 app.use('/api', indexRoutes);
