0c8ac8bd0c2fd874b83461b72ed8b0edf8707296
Jerast <jerast@github.com>
2024-04-24 08:34:35 -0500

    [PostList] fix issues

--- PostList/server/src/db.js
+++ PostList/server/src/db.js
@@ -3,6 +3,7 @@ import { DB_CNN } from './config.js';
 
 export const connectDB = async () => {
 	try {
+		await mongoose.set('strictQuery', false);
 		const database = await mongoose.connect(DB_CNN);
 		console.log('Connected to', database.connection.name);
 	} catch (error) {
